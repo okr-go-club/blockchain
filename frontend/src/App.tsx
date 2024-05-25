94906f0694962b0e017b1dc6ae3526d323963784
raisultan <ki.xbozz@gmail.com>
2024-05-26 00:00:11 +0500

    transaction request added 🎊

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,6 @@ export default function App() {
           <Route path="/transactions" element={
             <TransactionsPage
               caption={'Transactions Pool'}
-              transactions={transactions}
             />
           }
           />
--- frontend/src/TransactionsPage.tsx
+++ frontend/src/TransactionsPage.tsx
@@ -1,15 +1,66 @@
-import { Flex, Box } from '@chakra-ui/react';
-import TransactionsTable, { TransactionsTableProps } from './TransactionsTable';
+import { useEffect, useState } from 'react';
+import {
+    Flex, Box,
+    Spinner,
+} from '@chakra-ui/react';
+import TransactionsTable, { TransactionProps } from './TransactionsTable';
 import AddTransactionsModalButton from './AddTransactionModalButton';
+import ErrorAlert from './ErrorAlert';
+import axios from 'axios';
 
 
-export default function TransactionsPage({ caption, transactions }: TransactionsTableProps) {
+export default function TransactionsPage({ caption }: { caption: string }) {
+    const [transactions, setTransactions] = useState<TransactionProps[]>([]);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    async function fetchTransactions() {
+        const url = 'https://a5b00532-aa50-4792-9de0-834a9e550eff.mock.pstmn.io/transactions';
+        // const url = 'https://5a36e441-135d-4e1d-bd4b-410ad4e24cda.mock.pstmn.io/transactions';
+        setLoading(true);
+        try {
+            const response = await axios.get<TransactionProps[]>(url);
+            setTransactions(response.data);
+        } catch (error) {
+            console.error(error);
+            setError('Error fetching transactions!')
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchTransactions();
+    }, []);
+
     return (
         <Box>
-            <TransactionsTable caption={caption} transactions={transactions} />
-            <Flex justifyContent={'flex-end'} mt={4}>
-                <AddTransactionsModalButton />
-            </Flex>
+            {
+                loading
+                    ? (
+                        <Flex
+                            justifyContent="center"
+                            alignItems="center"
+                            height="100vh"
+                            width="100vw"
+                            position="fixed"
+                            top="0"
+                            left="0"
+                            zIndex="1000"
+                        >
+                            <Spinner size="xl" />
+                        </Flex>
+                    )
+                    : error
+                        ? <ErrorAlert message={error} />
+                        :
+                        <>
+                            <TransactionsTable caption={caption} transactions={transactions} />
+                            <Flex justifyContent={'flex-end'} my={6}>
+                                <AddTransactionsModalButton />
+                            </Flex>
+                        </>
+            }
         </Box>
     );
 };
