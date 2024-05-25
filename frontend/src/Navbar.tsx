3a3e16a43b2db2a42343df3aca36ac5c9b439aa9
raisultan <ki.xbozz@gmail.com>
2024-05-25 15:46:38 +0500

    active tab

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,16 +1,41 @@
+import React from "react";
 import { Flex, Link, Image } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useLocation } from "react-router-dom";
 import logo from './logo.png'
 
+const hoverBg = '#2e384b';
+const linkStyles: React.CSSProperties = {
+    textDecoration: 'none',
+    padding: '6px 20px 6px 20px',
+    color: 'white',
+    borderRadius: '10px',
+};
+
 export default function Navbar() {
+    const location = useLocation();
+
     return (
         <Flex my={'20px'} alignItems="center">
             <Image src={logo} alt="Logo" boxSize="50px" />
             <Flex pl={'20px'} gap={4}>
-                <Link fontSize={'18px'} as={RouterLink} to="/blocks" color="white">
+                <Link
+                    style={linkStyles}
+                    bg={location.pathname === '/blocks' ? hoverBg : ''}
+                    fontSize={'18px'}
+                    as={RouterLink}
+                    to="/blocks"
+                    color="white"
+                >
                     Blocks
                 </Link>
-                <Link fontSize={'18px'} as={RouterLink} to="/transactions" color="white">
+                <Link
+                    style={linkStyles}
+                    bg={location.pathname === '/transactions' ? hoverBg : ''}
+                    fontSize={'18px'}
+                    as={RouterLink}
+                    to="/transactions"
+                    color="white"
+                >
                     Transactions
                 </Link>
             </Flex>
