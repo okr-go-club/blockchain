26ac97501055e623011be99361a8cec2f4441435
ViktorShv95 <43659721+ViktorShv95@users.noreply.github.com>
2024-09-29 14:49:12 +0700

    Bind to a port

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -1,6 +1,11 @@
 package chain
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
 	"testing"
 )
 
@@ -111,9 +116,9 @@ func TestBlock_MineBlock(t *testing.T) {
 		difficulty int
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name    string
+		fields  fields
+		args    args
 		wantErr bool
 	}{
 		{
@@ -234,6 +239,143 @@ func TestBlock_MineBlock(t *testing.T) {
 			err := b.MineBlock(tt.args.difficulty)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Block.MineBlock() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+// func TestBlock_IsValid(t *testing.T) {
+// 	type fields struct {
+// 		Transactions []Transaction
+// 		Timestamp    int64
+// 		PreviousHash string
+// 		Nonce        int
+// 		Hash         string
+// 		Capacity     int
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		want   bool
+// 	}{
+// 		{
+// 			name: "empty block is invalid",
+// 			fields: fields{
+// 				Transactions: []Transaction{},
+// 			},
+// 			want: false,
+// 		},
+// 		{
+// 			name: "block with transactions is valid",
+// 			fields: fields{
+// 				Transactions: []Transaction{
+// 					{
+// 						FromAddress:   "123",
+// 						ToAddress:     "456",
+// 						Amount:        10.0,
+// 						TransactionId: "d6f1c4e6-9d7e-11eb-a8b3-0242ac130003",
+// 						Timestamp:     1643723400,
+// 						Signature:     b.Transactions[0].generateVerifiedSignature(),
+// 					},
+// 				},
+// 			},
+// 			want: true,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			b := &Block{
+// 				Transactions: tt.fields.Transactions,
+// 				Timestamp:    tt.fields.Timestamp,
+// 				PreviousHash: tt.fields.PreviousHash,
+// 				Nonce:        tt.fields.Nonce,
+// 				Hash:         tt.fields.Hash,
+// 				Capacity:     tt.fields.Capacity,
+// 			}
+// 			if got := b.IsValid(); got != tt.want {
+// 				t.Errorf("Block.IsValid() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
+
+func TestTransaction_verifySignature(t *testing.T) {
+	// Generate a private key
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Marshal the private key to PEM
+	privateKeyPEM := pem.EncodeToMemory(
+		&pem.Block{Type: "EC PRIVATE KEY", Bytes: x509.MarshalPKCS8PrivateKey(privateKey)},
+	)
+
+	type fields struct {
+		FromAddress   string
+		ToAddress     string
+		Amount        float64
+		Timestamp     int
+		TransactionId string
+		Signature     string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "valid signature",
+			fields: fields{
+				FromAddress:   "123",
+				ToAddress:     "456",
+				Amount:        10.0,
+				Timestamp:     1643723400,
+				TransactionId: "d6f1c4e6-9d7e-11eb-a8b3-0242ac130003",
+				Signature:     "",
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "invalid signature",
+			fields: fields{
+				FromAddress:   "123",
+				ToAddress:     "456",
+				Amount:        10.0,
+				Timestamp:     1643723400,
+				TransactionId: "d6f1c4e6-9d7e-11eb-a8b3-0242ac130003",
+				Signature:     "wrong signature",
+			},
+			want:    false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tr := &Transaction{
+				FromAddress:   tt.fields.FromAddress,
+				ToAddress:     tt.fields.ToAddress,
+				Amount:        tt.fields.Amount,
+				Timestamp:     tt.fields.Timestamp,
+				TransactionId: tt.fields.TransactionId,
+			}
+
+			// Sign the transaction using the private key
+			err = tr.Sign(string(privateKeyPEM))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// Verify the signature
+			got, err := tr.VerifySignature()
+			if err != nil {
+				t.Errorf("VerifySignature() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("VerifySignature() = %v, want %v", got, tt.want)
 			}
 		})
 	}
